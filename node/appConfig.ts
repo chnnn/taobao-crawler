3c30ca7c0cae936d7fb8d2b40253ba3bd6841680
schen0x <schen.git@gmail.com>
2021-03-02 23:34:06 +0900

    Selector

--- node/appConfig.ts
+++ node/appConfig.ts
@@ -1,18 +1,18 @@
-/** locating first item && screenshot */
-export const ItemSelector = 'dl.item'
+/** locating an item */
+export const SELECTORS_PRIME = {
+    item: 'dl.item'
+}
 
-/** ItemPicSelector.src is the url string to the 180x180 item img*/
-export const ItemPicSelector = 'dl.item > dt.photo img'
-
-/** find item detail info(the Item Name is its innerText) */
-export const ItemNameSelector = 'dl.item > dd.detail > a'
-
-// export const ItemCurrencySelector = 'dl.item > dd.detail > div.attribute > div.cprice-area > .symbol'
-
-/** price in CNY. (.innerText == '8.00') */
-export const ItemPriceSelector = 'dl.item > dd.detail > div.attribute > div.cprice-area > .c-price'
-
-/** (.innerText = '总销量: 1') */
-export const ItemSalesVolumeSelector = 'dl.item > dd.detail > div.attribute > div.sale-area'
-/** (.innerText = '评价: 2') */
-export const ItemRatingSelector = 'dl.item > dd.rates a'
+export const SELECTORS_SUB = {
+    /** ItemPicSelector.src is the url string to the 180x180 item img */
+    itemPic: 'dt.photo img',
+    /** .textContent is the item name. href is the item link */
+    itemNameAndLink: 'dd.detail > a',
+    // itemCurrency = 'dd.detail > div.attribute > div.cprice-area > .symbol'
+    /** price in CNY. (.textContent == '8.00') */
+    itemPrice: 'dd.detail > div.attribute > div.cprice-area > .c-price',
+    /** (.textContent = '总销量: 1') */
+    itemSalesVolume: 'dd.detail > div.attribute > div.sale-area',
+    /** (.textContent = '评价: 2') */
+    itemRating: 'dd.rates a',
+}
--- node/src/puppeteerMain.ts
+++ node/src/puppeteerMain.ts
@@ -1,13 +1,38 @@
 import puppeteer from 'puppeteer'
 import { readJSONFileToObj, autoScrollToBottom, scrollToSelector } from '@/src/helper'
+import { SELECTORS_PRIME, SELECTORS_SUB } from '@/appConfig'
 
+type ItemInfo = {
+  /** 180x180 pic */
+  itemPicUrl: string
+  itemName: string
+  itemLink: string
+  /** price in CNY '8.00' */
+  itemPrice: string
+  /** '总销量: 1' */
+  itemSalesVolume: string
+  /** '评价: 2' */
+  itemRating: string
+}
 type Page = puppeteer.Page
+const ERRSTR = 'error'
 export default async function puppetHandler() {
   // const browser = await puppeteer.launch({headless: false, executablePath: '/usr/bin/chromium'});
   const browser = await puppeteer.launch({ headless: false });
   const page: Page = await browser.newPage();
+  await cookiesSetup(page)
   await browse(page)
   // TODO close browser
+}
+const cookiesSetup = async (page: Page) => {
+  const cookiesObj = readJSONFileToObj()
+  for (const [name, val] of Object.entries(cookiesObj.cookies)) {
+    await page.setCookie({ name: name, value: val })
+  }
+
+  await page.waitForNavigation({
+    waitUntil: 'load'
+  })
 }
 
 const browse = async (page: Page) => {
@@ -24,19 +49,38 @@ const browse = async (page: Page) => {
 
   await autoScrollToBottom(page)
 
-  //TODO to item
-  await scrollToSelector(page, '')
-  //TODO document.querySelectorAll(selector) loop
-  
+  await scrollToSelector(page, SELECTORS_PRIME.item)
+  const shopName = 's01'
+  await page.screenshot({ path: `${shopName}_Result.png` })
 
-  const cookiesObj = readJSONFileToObj()
-  for (const [name, val] of Object.entries(cookiesObj.cookies)) {
-    await page.setCookie({ name: name, value: val })
+  const items = document.querySelectorAll(SELECTORS_PRIME.item)
+  const itemsList: Element[] = []
+  items.forEach((item) => { itemsList.push(item) })
+
+  /** top 10 */
+  const targetItems = itemsList.slice(0, 10)
+
+  const itemsInfo: ItemInfo[] = []
+  for (const item of targetItems) {
+    const imgSelect = item.querySelector(SELECTORS_SUB.itemPic)
+    const imgURL = imgSelect ? imgSelect['src'] : ERRSTR
+    const nameAndLinkSelect = item.querySelector(SELECTORS_SUB.itemNameAndLink)
+    const name = nameAndLinkSelect ? nameAndLinkSelect.textContent : ERRSTR
+    const link = nameAndLinkSelect ? nameAndLinkSelect['href'] : ERRSTR
+    const priceSelect = item.querySelector(SELECTORS_SUB.itemPrice)
+    const price = priceSelect ? priceSelect.textContent : ERRSTR
+    const ratingSelect = item.querySelector(SELECTORS_SUB.itemRating)
+    const rating = ratingSelect ? ratingSelect.textContent : ERRSTR
+    const salesVolumeSelect = item.querySelector(SELECTORS_SUB.itemSalesVolume)
+    const salesVolume = salesVolumeSelect ? salesVolumeSelect.textContent : ERRSTR
+    itemsInfo.push({
+      itemPicUrl: imgURL || '',
+      itemName: name,
+      itemLink: link,
+      itemPrice: price,
+      itemRating: rating,
+      itemSalesVolume: salesVolume,
+    })
   }
-
-  await page.waitForNavigation({
-    waitUntil: 'load'
-  })
-  await page.screenshot({ path: 'example.png' });
-  // TODO extract info from 
+  fetch('')
 }
